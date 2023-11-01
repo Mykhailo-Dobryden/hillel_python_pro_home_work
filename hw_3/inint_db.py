7b50755892b65be06c76b5f9e76b3b2cbc291856
Mykhailo Dobryden <dobrydenmykhail@gmail.com>
2023-11-01 15:11:47 +0200

    docstring was added

--- hw_3/inint_db.py
+++ hw_3/inint_db.py
@@ -1,6 +1,19 @@
 import sqlite3
 
 def init_db(db_name: str, schema: str):
+    """
+        Initializes an SQLite database by creating tables and executing the schema definition.
+
+        Args:
+            db_name (str): The name of the SQLite database to be initialized.
+            schema (str): The path to the schema file containing SQL script for creating a table.
+
+        This function creates an SQLite database with the specified name and executes the SQL script defined in the provided schema file.
+        The schema file typically contains table creation and schema definitions. After executing the script,
+        the function commits the changes and closes the database connection.
+
+        Note: Make sure to pass the correct paths to the database name and schema file.
+    """
     connection = sqlite3.connect(db_name)
 
     with open(schema) as f:
